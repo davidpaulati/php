d288dd2c934de217f9a832baf47ebe0ba8f60718
davidpaulati <32557397+davidpaulati@users.noreply.github.com>
2020-12-28 14:53:41 -0300

    Add files via upload
    exercicio

---
+++ calendario_dia_negrito.php
@@ -0,0 +1,76 @@
+<?php
+    function linha($semana)
+    {
+        $linha = '<tr>';
+        for($i = 0; $i <= 6; $i++) {
+            if(array_key_exists($i, $semana)){
+                $linha .= "<td>{$semana[$i]}</td>";
+            } else {
+                $linha .= "<td></td>";
+            }
+        }
+        $linha .= '<tr>';
+
+        return $linha;
+    }
+
+
+
+    function calendario()
+    {
+        $calendario = '';
+        $dia = 1;
+        $semana = [];
+        while ($dia <= 31){
+            array_push($semana, $dia);
+
+            if (count($semana)== 7){
+                $calendario .= linha($semana);
+                $semana = [];
+            }
+
+            $dia++;
+        }
+        $calendario .= linha($semana);
+        return $calendario;
+    }
+    
+    function saudacao()
+    {
+        $hora = date(" H ");
+        if($hora >= 12 && $hora <18){
+            $resposta ="Boa tarde";
+        }
+        else if ($hora >=0 && hora < 12){
+            $resposta  ="Bom dia";
+        }
+        else{
+            $resposta  ="Boa Noite";
+        }
+        echo $resposta;
+    }
+
+    function hora()
+    {
+        $hora = date('H:i:s');
+        echo $hora;
+    }
+?>
+
+
+
+
+<table border ="1">
+    <?php echo hora();?>
+    <?php echo saudacao();?>
+    <tr>
+        <th>Dom</th>
+        <th>Seg</th>
+        <th>Ter</th>
+        <th>Qua</th>
+        <th>Qui</th>
+        <th>Sex</th>
+        <th>Sab</th>
+    </tr>
+    <?php echo calendario(); ?>
+</table>
